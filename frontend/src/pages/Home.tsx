6fb536c7c3f304631aae676e1d7d4db069a6d1c3
willlhortala-art <willlhortala@gmail.com>
2026-09-09 13:47:16 +0200

    Remove unused VehicleModel import from Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { VEHICLES, EQUIPMENTS, type VehicleModel } from './data';
+import { VEHICLES, EQUIPMENTS } from './data';
 
 export default function Home() {
   const [selectedVehicleId, setSelectedVehicleId] = useState<string>(VEHICLES[0].id);
